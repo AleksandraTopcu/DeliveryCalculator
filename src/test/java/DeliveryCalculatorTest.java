016740621957f0dd639bee6fe115ef130e329f9c
Aleksandra Topchu <aineumestova@avito.ru>
2025-02-17 15:58:50 +0300

    Unit-tests

--- src/test/java/DeliveryCalculatorTest.java
+++ src/test/java/DeliveryCalculatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
